fbfdead0447d824b0f9c610350d18a4b28fc8d6b
bigmeow <runnning@vip.163.com>
2017-08-23 15:14:09 +0800

    完善工作流

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,6 +1,10 @@
 const express=require('express');
 const router=express.Router();
 
+router.get('/', (req, res, next) => {
+    res.render('index');
+});
+
 router.get('/index',(req,res,next)=>{
     res.render('index');
 })
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,69 @@
-const webpack=require('webpack');
-const path=require('path');
+const webpack = require('webpack');
+const path = require('path');
 //源码目录
-const srcPath=path.resolve(__dirname,'src');
+const srcPath = path.resolve(__dirname, 'src');
 
 
 
-module.exports={
-    entry:{
-        'common/main':[srcPath+'/common/main.js','webpack-hot-middleware/client?reload=true']//4  指定重载策略，修改了前端代码js,css后，浏览器会自动刷新
+module.exports = {
+    entry: {
+        'common/main': [srcPath + '/common/main.js', 'webpack-hot-middleware/client?reload=true'] //4  指定重载策略，修改了前端代码js,css后，浏览器会自动刷新
     },
-    output:{
-        path:__dirname+'/public',
-        filename:'[name].js',
-        publicPath:'http://localhost:8080/public'
+    output: {
+        path: __dirname + '/public',
+        filename: '[name].js',
+        publicPath: 'http://localhost:3000/public', //这里的地址要换成代理服务器的地址
     },
-    devtool:'eval-source-map',//2
-    module:{
-        rules:[
-            {
-                test:/\.(png|jpg)$/,
-                //如果图片小于8k  8192byte  就将图片Base64编码成字符串
-                use:'url-loader?limit=8192&context=client&name=[name].[ext]'
+    devtool: 'eval-source-map', //2
+    resolve:{
+        //取别名，在自己的js里面直接使用这个别名
+        alias: {
+            SRC:srcPath ,
+        }
+    },
+    module: {
+        rules: [{
+                test: /\.(png|jpg)$/,
+                use: 'url-loader'
             },
             {
-                test:/\.css$/,
-                use:[
+                test: /\.css$/,
+                use: [
                     'style-loader',
                     'css-loader?sourceMap' //2
                 ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env'],
+                        plugins: ['transform-runtime']
+                    }
+                }
             }
 
 
         ]
     },
-    plugins:[
-        new webpack.optimize.OccurrenceOrderPlugin(),//根据模块的调用次数，给模块分配id，使得id可预测，降低文件大小 
+    plugins: [
+        //把jquery的全局变量提取出来的插件(jQuery not undefined)
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        }),
+        new webpack.optimize.OccurrenceOrderPlugin(), //根据模块的调用次数，给模块分配id，使得id可预测，降低文件大小 
         new webpack.HotModuleReplacementPlugin(), // 1.启用 HMR，模块热替换
         new webpack.NoEmitOnErrorsPlugin() //报错但不退出webpack的进程
     ]
 
 
 };
-
-
